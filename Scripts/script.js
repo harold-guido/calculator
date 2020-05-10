ce51098d4dfaa6169778fddb1e4b964681bb5072
Harold Parra <gc.harold.parra@gmail.com>
2020-05-10 18:49:27 +0100

    automated the making of number buttons using javascript

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,13 @@
-var numOne = 0;
-var numTwo = 0;
+// Creating Number Button Divs
+const keypad = document.querySelector(".keypad");
+
+for (let i = 0; i < 10; i++) {
+  let numButton = document.createElement("div");
+  numButton.classList.add("numButton");
+  numButton.id = "n" + i;
+  numButton.textContent = i;
+  keypad.appendChild(numButton);
+}
 
 // Operations Function
 
@@ -9,3 +17,5 @@ const operations = {
   tim: (a, b) => a * b,
   div: (a, b) => a / b,
 };
+
+// Wiring Buttons to Script
