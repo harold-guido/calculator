71523afa2ecf879ffd402dc5d8907466011a8c00
Harold Parra <gc.harold.parra@gmail.com>
2020-05-26 19:01:08 +0100

    one operation is functional, continuous operations are not functional

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 // Giving  numButtons functionality
 
 for (i = 0; i < 10; i++) {
-  numButton = document.getElementById("n" + i);
+  let numButton = document.getElementById("n" + i);
   numButton.onclick = (item) => {
-    screenContent = document.querySelector(".screen");
-    screenContent.textContent =
-      screenContent.textContent + item.target.textContent;
+    let screenContent = document.querySelector(".screen");
+    if (operating == true) {
+      screenContent.textContent = item.target.textContent;
+      operating = false;
+    } else {
+      screenContent.textContent =
+        screenContent.textContent + item.target.textContent;
+    }
   };
 }
 
 // Giving reset button functionality
 
-resetButton = document.getElementById("reset");
+let resetButton = document.getElementById("reset");
 resetButton.onclick = () => {
   document.querySelector(".screen").textContent = "";
+  operationCounter = 0;
+  storedValues = {};
 };
 
 // Operations Function
@@ -23,6 +30,44 @@ const operations = {
   min: (a, b) => a - b,
   tim: (a, b) => a * b,
   div: (a, b) => a / b,
+  equal: (a, b) => b,
+  reset: (a, b) => "",
 };
 
-// Wiring Buttons to Script
+const operate = (operation, num1, num2) => {
+  return operations[operation](Number(num1), Number(num2)).toString();
+};
+
+// wiring buttons to script
+
+// let plusop = document.querySelector("#add");
+// let minop = document.querySelector("#min");
+// let timOp = document.querySelector("#tim");
+// let divOp = document.querySelector("#div");
+// let equalOp = document.querySelector("#equal");
+
+let operationCounter = 0;
+let storedValues = {};
+let operating = false;
+
+let operationButtons = document.querySelectorAll(".operation");
+
+operationButtons.forEach((node) => {
+  node.addEventListener("click", () => {
+    operating = true;
+    operationCounter += 1;
+    storedValues["operation" + operationCounter] = [
+      node.id,
+      Number(document.querySelector(".screen").textContent),
+    ];
+    document.querySelector(".screen").textContent = "";
+
+    if (operationCounter > 1) {
+      document.querySelector(".screen").textContent = operate(
+        storedValues["operation" + (operationCounter - 1)][0],
+        storedValues["operation" + (operationCounter - 1)][1],
+        storedValues["operation" + operationCounter][1]
+      );
+    }
+  });
+});
