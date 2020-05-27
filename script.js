a8e64f65ffd3ff01eaec1a8694ea80cea41c2709
Harold Parra <gc.harold.parra@gmail.com>
2020-05-27 18:29:57 +0100

    gave functionality to the backspace button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// Giving reset button functionality
+let operationCounter = 0;
+let storedValues = {};
+let operating = true;
 
 let resetButton = document.getElementById("reset");
 resetButton.addEventListener("click", () => {
@@ -8,7 +10,42 @@ resetButton.addEventListener("click", () => {
   operating = true;
 });
 
-// Operations Function
+for (i = 0; i < 10; i++) {
+  let numButton = document.getElementById("n" + i);
+  numButton.onclick = (number) => {
+    let screenContent = document.querySelector(".screen");
+    if (operating == true) {
+      screenContent.textContent = number.target.textContent;
+      operating = false;
+    } else {
+      screenContent.textContent += number.target.textContent;
+    }
+  };
+}
+
+let dotButton = document.getElementById("dot");
+dotButton.onclick = (dot) => {
+  let screenContent = document.querySelector(".screen");
+  if (operating != true) {
+    screenContent.textContent += dot.target.textContent;
+  }
+};
+
+let backSpace = document.getElementById("backspace");
+backSpace.onclick = () => {
+  let screenContent = document.querySelector(".screen");
+  if (operating != true && screenContent.textContent.length > 0) {
+    screenContent.textContent = removeLastChar(screenContent.textContent);
+  }
+};
+const removeLastChar = (string) => {
+  workingString = " " + string;
+  charArray = workingString.split("");
+  charArray.pop();
+  return charArray.join("");
+};
+
+let operationButtons = document.querySelectorAll(".operation");
 
 const operations = {
   add: (a, b) => Math.round((a + b) * 100000) / 100000,
@@ -21,12 +58,6 @@ const operations = {
 const operate = (operation, num1, num2) => {
   return operations[operation](Number(num1), Number(num2)).toString();
 };
-
-let operationCounter = 0;
-let storedValues = {};
-let operating = true;
-
-let operationButtons = document.querySelectorAll(".operation");
 
 operationButtons.forEach((operation) => {
   if (operation.id != "reset") {
@@ -59,34 +90,3 @@ operationButtons.forEach((operation) => {
     };
   }
 });
-// Giving  numButtons functionality
-
-for (i = 0; i < 10; i++) {
-  let numButton = document.getElementById("n" + i);
-  numButton.onclick = (number) => {
-    let screenContent = document.querySelector(".screen");
-    if (operating == true) {
-      screenContent.textContent = number.target.textContent;
-      operating = false;
-    } else {
-      screenContent.textContent += number.target.textContent;
-    }
-  };
-}
-
-let dotButton = document.getElementById("dot");
-let backSpace = document.getElementById("backspace");
-
-dotButton.onclick = (dot) => {
-  let screenContent = document.querySelector(".screen");
-  if (operating != true) {
-    screenContent.textContent += dot.target.textContent;
-  }
-};
-// wiring buttons to script
-
-// let plusop = document.querySelector("#add");
-// let minop = document.querySelector("#min");
-// let timOp = document.querySelector("#tim");
-// let divOp = document.querySelector("#div");
-// let equalOp = document.querySelector("#equal");
